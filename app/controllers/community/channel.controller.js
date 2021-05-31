ef1b91a3b380f959d226b001eccdb6568d03af33
Leonardo Bueno <leonardofibbauru@gmail.com>
2021-05-31 15:11:46 -0300

    removendo channel

--- app/controllers/community/channel.controller.js
+++ app/controllers/community/channel.controller.js
@@ -37,5 +37,52 @@ module.exports = {
             })
         }
 
+    },
+    async delete(req, res) {
+        try {
+            const { channel } = req.params
+            const user = req.user
+
+            let channelFind = await Channel.findOne({
+                attributes: ['id', 'name', 'slug', 'createdAt', 'updatedAt'],
+                where: {
+                    channel: channel,
+                    user: user.id
+                }
+            })
+
+            if (empty(channelFind)) {
+                throw ("Ocorreu um erro ao remover Channel!");
+            }
+
+            Channel.update({
+                excluded: 1,
+            }, {
+                where: {
+                    channel: channel,
+                    user: user.id
+                }
+            }).then(async (t) => {
+                await Replies.update({
+                    excluded: 1,
+                }, {
+                    where: {
+                        channel: channel
+                    }
+                })
+            })
+
+            res.status(200).json({
+                status: true,
+                message: "Channel excluído com sucesso!!!"
+            })
+
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({
+                status: false,
+                message: e
+            })
+        }
     }
 }
