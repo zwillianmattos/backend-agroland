808ee8e16f8851fa5f0bc4eba99cdfcc9745de83
Willian de Mattos Silva <willian_matt@hotmail.com>
2021-08-03 22:24:28 -0300

    hotfix: connection database production

--- app/config/database.js
+++ app/config/database.js
@@ -8,17 +8,17 @@ module.exports = {
         port: 3308
     },
     test: {
-        username: '',
-        password: '',
-        database: '',
-        host: '',
-        dialect: 'mysql'
-    },
-    production: {
         username: 'u975537847_plantcare',
         password: '4KlbmHmwDk!',
         database: 'u975537847_plantcare',
         host: 'sql124.main-hosting.eu',
+        dialect: 'mysql'
+    },
+    production: {
+        username: 'u975537847_root',
+        password: '4KlbmHmwDk!',
+        database: 'u975537847_plant',
+        host: '85.10.205.173',
         dialect: 'mysql'
     }
 };
